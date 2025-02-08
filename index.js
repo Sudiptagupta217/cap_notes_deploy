0c33f2480cd07fef5ac0f045e5eb3075f0b88dde
Sudipta Gupta <sudiptagupta217@gmail.com>
2025-02-08 22:28:39 +0530

    updated the notes App and deploying

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const cors = require("cors")
 
 const app = express()
 
-app.use(express.json())
 app.use(cors({
-    origin: '*', // Allow all origins
+    origin: 'http://localhost:5173', // Allow specific origin
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
-    allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+    // allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
     credentials: true // Allow credentials
 }))
 
+app.use(express.json())
 
 app.use("/users", userRouter)
 app.use("/notes", noteRouter)
