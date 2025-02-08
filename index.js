e277200e4006825e55220c32d840c6c7ecece232
Sudipta Gupta <sudiptagupta217@gmail.com>
2025-02-08 21:57:39 +0530

    updated the notes App and deploying

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: "http://localhost:5173",  // Allow requests from your React frontend
+    origin: ["http://localhost:5173", "https://cap-notes-deploy.onrender.com"],  // Allow requests from your React frontend and deployed backend
     credentials: true,  // Allow cookies & authorization headers
     methods: ["GET", "POST", "PUT", "DELETE"],  // Allowed request types
-    allowedHeaders: ["Content-Type", "Authorization"]  // Explicitly allow headers
+    allowedHeaders: ["Content-Type", "Authorization"],  // Explicitly allow headers
+    exposedHeaders: ["Access-Control-Allow-Origin"]  // Expose headers to the client
 }));
 
 
