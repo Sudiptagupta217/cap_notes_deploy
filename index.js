b0ca60061899f3b910504230c4b9c317ad1c1a56
Sudipta Gupta <sudiptagupta217@gmail.com>
2025-02-08 22:16:23 +0530

    updated the notes App and deploying

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: '*', // Allow all origins
+    origin: 'http://localhost:5173', // Allow specific origin
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization'] // Allow specific headers
 }))
